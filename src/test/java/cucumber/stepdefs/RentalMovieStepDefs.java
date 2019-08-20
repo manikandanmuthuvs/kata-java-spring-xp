fce7e8949c89a4f47403b89f296aa6dd3a3e7f40
Manikandan Muthuvs <manikandan.muthuvs@gmail.com>
2019-08-20 10:21:14 +0530

    checking travis deployment in heroku

--- src/test/java/cucumber/stepdefs/RentalMovieStepDefs.java
+++ src/test/java/cucumber/stepdefs/RentalMovieStepDefs.java
@@ -8,6 +8,7 @@ import cucumber.api.java.en.When;
 public class RentalMovieStepDefs {
     @Given("^the new movies are available$")
     public void the_new_movies_are_available() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
